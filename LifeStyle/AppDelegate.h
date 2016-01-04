ae841b412c3e5dc08224f06c58e41e459bdf162d
hzf <huangzhifei2009@126.com>
2016-01-04 10:27:02 +0800

    Initial Commit

---
+++ LifeStyle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LifeStyle
+//
+//  Created by huangzhifei on 16/1/4.
+//  Copyright © 2016年 huangzhifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LifeStyle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LifeStyle
+//
+//  Created by huangzhifei on 16/1/4.
+//  Copyright © 2016年 huangzhifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
